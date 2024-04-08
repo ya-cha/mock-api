87e6f8b80853a0a1125ec0e8225588f632233319
Jascha <jg@23m.com>
2024-04-08 15:50:19 +0200

    Handle endpoint requests 🐾

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', \App\Livewire\Endpoints::class);
+
+Route::fallback(function () {
+    $endpoint = \App\Models\Endpoint::where('request_uri', request()->getRequestUri())
+        ->where('request_method', request()->getMethod())
+        ->firstOrCreate([
+            'request_method' => request()->getMethod(),
+            'request_uri' => request()->getRequestUri(),
+        ], [
+            'response_status_code' => 501,
+            'response_headers' => [],
+            'response_body' => 'Not Implemented. Please define this endpoint.',
+        ]);
+
+    return response($endpoint->response_body, $endpoint->response_status_code, $endpoint->headers);
 });
+
