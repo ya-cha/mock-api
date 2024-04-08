bc7aac3a734c21b10749921a1b15fd194f59a2c8
Jascha <jg@23m.com>
2024-04-08 23:04:10 +0200

    Use description list for endpoints

--- resources/views/livewire/endpoints.blade.php
+++ resources/views/livewire/endpoints.blade.php
@@ -1,25 +1,25 @@
 <div class="container mx-auto">
 @foreach($endpoints as $endpoint)
     <div class="flex py-3 border-b justify-between space-x-12">
-        <div class="flex-1">
-            <div class="font-bold text-gray-300 text-xs uppercase">Request</div>
-            <div class="flex items-baseline space-x-3">
+        <dl class="flex-1">
+            <dt class="font-bold text-gray-300 text-xs uppercase">Request</dt>
+            <dd class="flex items-baseline space-x-3">
                 <div class="font-bold text-gray-500">{{ $endpoint->request_method }}</div>
                 <div class="font-mono text-sm">{{ $endpoint->request_uri }}</div>
-            </div>
-        </div>
-        <div class="flex-1">
-            <div class="font-bold text-gray-300 text-xs uppercase">Status Code</div>
-            <div class="font-bold text-gray-500">{{ $endpoint->response_status_code }}</div>
-            <div class="font-bold text-gray-300 text-xs uppercase mt-3">Headers</div>
-            <div class="font-mono text-sm">
+            </dd>
+        </dl>
+        <dl class="flex-1">
+            <dt class="font-bold text-gray-300 text-xs uppercase">Status Code</dt>
+            <dd class="font-bold text-gray-500">{{ $endpoint->response_status_code }}</dd>
+            <dt class="font-bold text-gray-300 text-xs uppercase mt-3">Headers</dt>
+            <dd class="font-mono text-sm">
                 @foreach($endpoint->response_headers as $key => $value)
                     <div>{{ $key }}: {{ $value }}</div>
                 @endforeach
-            </div>
-            <div class="font-bold text-gray-300 text-xs uppercase mt-3">Body</div>
-            <div class="font-mono text-sm">{{ $endpoint->response_body }}</div>
-        </div>
+            </dd>
+            <dt class="font-bold text-gray-300 text-xs uppercase mt-3">Body</dt>
+            <dd class="max-h-32 font-mono text-sm overflow-hidden">{{ $endpoint->response_body }}</dd>
+        </dl>
     </div>
 @endforeach
 {{ $endpoints->links() }}
