f79b13709271bf288d9e75719705fd5de813fe40
Jascha <jg@23m.com>
2024-04-08 17:11:49 +0200

    Add pagination

--- app/Livewire/Endpoints.php
+++ app/Livewire/Endpoints.php
@@ -3,12 +3,15 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Endpoints extends Component
 {
+    use WithPagination;
+
     public function render()
     {
-        $endpoints = \App\Models\Endpoint::all();
+        $endpoints = \App\Models\Endpoint::latest()->paginate(5);
 
         return view('livewire.endpoints', compact('endpoints'));
     }
--- resources/views/livewire/endpoints.blade.php
+++ resources/views/livewire/endpoints.blade.php
@@ -22,6 +22,7 @@
         </div>
     </div>
 @endforeach
+{{ $endpoints->links() }}
 </div>
 
 
