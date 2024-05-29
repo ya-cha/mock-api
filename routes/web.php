e0c1b56fb370c9ad04db5a5a030969fd494b1607
Jascha <jg@23m.com>
2024-05-29 18:21:34 +0200

    Allow all route methods

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', \App\Livewire\Endpoints::class);
 
-Route::fallback(function () {
+Route::any('/{any}', function () {
     $endpoint = \App\Models\Endpoint::where('request_uri', request()->getRequestUri())
         ->where('request_method', request()->getMethod())
         ->firstOrCreate([
@@ -16,6 +16,6 @@ Route::fallback(function () {
             'response_body' => 'Not Implemented. Please define this endpoint.',
         ]);
 
-    return response($endpoint->response_body, $endpoint->response_status_code, $endpoint->headers);
-});
+    return response($endpoint->response_body, $endpoint->response_status_code, $endpoint->response_headers->toArray());
+})->where('any', '.*');
 
